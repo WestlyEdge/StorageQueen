2eac6d60f5b4fa1e7964a2fa92a403684677cebd
wes <westly.edge@gmail.com>
2022-09-09 02:55:04 -0500

    removed auto scaling group from teamcity installation

--- cloudformation/teamcity/scripts/deploy-stack.sh
+++ cloudformation/teamcity/scripts/deploy-stack.sh
@@ -11,17 +11,10 @@ aws cloudformation deploy \
 --template-file $TEMPLATE_FILE \
 --capabilities "CAPABILITY_NAMED_IAM" \
 --parameter-overrides \
-VpcId="vpc-0076efa73224f9c6d" \
-SubnetIds="subnet-0d2ec6c687fd344d1, subnet-0063d85ef0f22f5ef, subnet-021efa10cf691557c, subnet-0dd04f53960f71b00, subnet-08f66bdba40d62cd2, subnet-09de3e8f762a260f6" \
 InstanceType="t2.medium" \
 ImageId="ami-05fa00d4c63e32376" \
 KeyPairName="storage-queen-keypair" \
-AutoScalingGroupMinSize="1" \
-AutoScalingGroupMaxSize="1" \
-ALBListenerPort=80 \
-ALBListenerProtocol="HTTP" \
-EC2HostPort=8112 \
-EC2HostProtocol="HTTP" \
+EC2HostPort=80 \
 TeamCityVersion="2022.04.3";
 
 
