9a3f09c51e34a8199c4a29d7b43799e5e4936a39
wes <westly.edge@gmail.com>
2022-09-11 18:59:24 -0500

    added script for teamcity to login to docker hub before pushing images

--- docker/scripts/docker-hub-login.sh
+++ docker/scripts/docker-hub-login.sh
@@ -3,4 +3,4 @@
 LOGIN_NAME=${1} # docker hub login name arg must be passed in as the first input arg
 LOGIN_ACCESS_TOKEN=${2} # docker hub login access token arg must be passed in as the second input arg
 
-docker login --u=$LOGIN_NAME --password=$LOGIN_ACCESS_TOKEN;
+docker login --username=$LOGIN_NAME --password=$LOGIN_ACCESS_TOKEN;
