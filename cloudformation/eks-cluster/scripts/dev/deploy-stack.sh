a3e0bd59342d7ad4c0664387f8b4f6321fd9de14
wes <westly.edge@gmail.com>
2022-09-13 21:27:58 -0500

    added eks cluster cloudformation stack

---
+++ cloudformation/eks-cluster/scripts/dev/deploy-stack.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+REGION="us-east-1";
+ENV="dev";
+STACK_NAME="sq-$ENV-eks-cluster";
+SCRIPT_DIR=$(dirname "$0");
+TEMPLATE_FILE=$SCRIPT_DIR/../../template.yaml;
+
+aws cloudformation deploy \
+--region $REGION \
+--stack-name $STACK_NAME \
+--template-file $TEMPLATE_FILE \
+--tags Env=$ENV \
+--capabilities "CAPABILITY_NAMED_IAM" \
+--parameter-overrides \
+Env=$ENV \
+NetworkingStackName="sq-$ENV-networking" \
+KubernetesVersion="1.23";
