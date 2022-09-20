df4f2ebcc1cafc76d6f20f0bcd1ea3c1bb3917e6
wes <westly.edge@gmail.com>
2022-09-20 01:02:03 -0500

    install prometheus into the eks cluster

--- helm/helm-installs.sh
+++ helm/helm-installs.sh
@@ -10,9 +10,19 @@ then
     exit 1; # terminate and indicate error
 fi
 
-# alb ingress controller
+# install alb ingress controller
 helm upgrade "aws-load-balancer-controller" \
 "https://aws.github.io/eks-charts/aws-load-balancer-controller-1.4.4.tgz" \
 --namespace "kube-system" \
 --values "$VALUES_DIR/aws-load-balancer-controller.yaml" \
 --install;
+
+# install rometheus
+helm upgrade "prometheus" \
+"https://github.com/prometheus-community/helm-charts/releases/download/prometheus-15.12.2/prometheus-15.12.2.tgz" \
+--values "$VALUES_DIR/prometheus.yaml" \
+--install;
+
+#kubectl annotate pods --all --overwrite --namespace "kube-system" prometheus.io/scrape="true";
+#kubectl annotate pods --all --overwrite --namespace "kube-system" prometheus.io/path="/metrics";
+#kubectl annotate pods --all --overwrite --namespace "kube-system" prometheus.io/port="8080";
